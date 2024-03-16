2dbce6035534467a5907efc42cb2e556b722c735
Oussama Moussaoui <omoussaoui1337@gmail.com>
2024-03-16 15:12:45 +0000

    chore: start search field

--- lib/presentation/screens/characters.dart
+++ lib/presentation/screens/characters.dart
@@ -4,7 +4,6 @@ import 'package:flutter_rickandmorty/data/models/character.dart';
 import 'package:flutter_rickandmorty/logic/cubit/characters_cubit.dart';
 import 'package:flutter_rickandmorty/presentation/widgets/character_card.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
-import 'package:velocity_x/velocity_x.dart';
 
 class CharactersScreen extends StatefulWidget {
   const CharactersScreen({super.key});
@@ -16,12 +15,15 @@ class CharactersScreen extends StatefulWidget {
 
 class _CharactersScreenState extends State<CharactersScreen> {
   List<Character> _characters = [];
-  final scrollController = ScrollController();
+  final _scrollController = ScrollController();
+  List<Character> _searchedCharacters = [];
+  final _searchTextController = TextEditingController();
+  bool _isSearching = false;
 
   void setupScrollController() {
-    scrollController.addListener(() {
-      if (scrollController.position.pixels ==
-          scrollController.position.maxScrollExtent) {
+    _scrollController.addListener(() {
+      if (_scrollController.position.pixels ==
+          _scrollController.position.maxScrollExtent) {
         BlocProvider.of<CharactersCubit>(context).loadCharacters();
       }
     });
@@ -34,16 +36,51 @@ class _CharactersScreenState extends State<CharactersScreen> {
     BlocProvider.of<CharactersCubit>(context).loadCharacters();
   }
 
+  // void filterCharacters() {
+  //   String query = _searchTextController.text.toLowerCase();
+  //   _isSearching = true;
+  //   _searchedCharacters = _characters.filter((character) {
+  //     String name = character.name.toLowerCase();
+  //     return name.startsWith(query);
+  //   }).toList();
+
+  //   setState(() {});
+  // }
+
+  Widget buildCharactersSearch() {
+    return TextField(
+      controller: _searchTextController,
+      keyboardType: TextInputType.text,
+      cursorColor: Colors.grey[900],
+      decoration: InputDecoration(
+        hintText: 'Find a character...',
+        hintStyle: TextStyle(
+          color: Colors.grey[900],
+          fontSize: 18,
+        ),
+        border: InputBorder.none,
+        filled: true,
+        fillColor: Colors.amber,
+        contentPadding: const EdgeInsets.all(10),
+      ),
+      style: const TextStyle(
+        fontSize: 20,
+        color: Colors.black,
+      ),
+      // onChanged: () {},
+    );
+  }
+
   Widget buildLoadedList({required bool showProgress}) {
     return Scrollbar(
-      controller: scrollController,
+      controller: _scrollController,
       thickness: 5,
       child: GridView.builder(
-        controller: scrollController,
-        padding: const EdgeInsets.symmetric(horizontal: 15),
+        controller: _scrollController,
+        padding: const EdgeInsets.fromLTRB(15, 10, 15, 0),
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
-          childAspectRatio: 3 / 4,
+          childAspectRatio: 5 / 6,
           crossAxisSpacing: 20,
           mainAxisSpacing: 30,
         ),
@@ -91,11 +128,15 @@ class _CharactersScreenState extends State<CharactersScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: 'Characters'.text.make(),
+        // title: 'Characters'.text.make(),
+        title: buildCharactersSearch(),
+        shadowColor: Colors.grey,
+        scrolledUnderElevation: 10,
+        toolbarHeight: 75,
       ),
       body: SafeArea(
         child: Padding(
-          padding: const EdgeInsets.fromLTRB(5, 20, 5, 5),
+          padding: const EdgeInsets.fromLTRB(5, 0, 5, 5),
           child: buildBlocWidget(),
         ),
       ),
