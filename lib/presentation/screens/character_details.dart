f8ae2559371f80a2a5937d603180213839f6fc11
Oussama Moussaoui <omoussaoui1337@gmail.com>
2024-03-16 23:05:14 +0000

    chore: start character bar animation

--- lib/presentation/screens/character_details.dart
+++ lib/presentation/screens/character_details.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_rickandmorty/data/models/character.dart';
+import 'package:flutter_rickandmorty/presentation/widgets/character_name_decoration.dart';
 import 'package:velocity_x/velocity_x.dart';
 
 class CharacterDetailsScreen extends StatelessWidget {
@@ -8,8 +9,38 @@ class CharacterDetailsScreen extends StatelessWidget {
   static const routeName = '/character';
   final Character character;
 
+  Widget buildSliverAppBar() {
+    return SliverAppBar(
+      expandedHeight: 600,
+      pinned: true,
+      stretch: true,
+      backgroundColor: Colors.grey[900],
+      flexibleSpace: FlexibleSpaceBar(
+        // title: character.name.text.gray900.make(),
+        title: SizedBox(
+          width: double.infinity,
+          child: CharacterNameDecoration(name: character.name),
+        ),
+        titlePadding: const EdgeInsets.all(0),
+        background: Hero(
+          tag: character.id,
+          child: Image.network(
+            character.image,
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return character.name.text.makeCentered();
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          buildSliverAppBar(),
+        ],
+      ),
+    );
   }
 }
