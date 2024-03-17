e77584c517adac7b2df85a6a42b5782123d5c642
Oussama Moussaoui <omoussaoui1337@gmail.com>
2024-03-17 15:31:31 +0000

    chore: add character details screen

--- lib/presentation/screens/character_details.dart
+++ lib/presentation/screens/character_details.dart
@@ -9,12 +9,43 @@ class CharacterDetailsScreen extends StatelessWidget {
   static const routeName = '/character';
   final Character character;
 
-  Widget buildSliverAppBar() {
+  Widget buildHeadlineText(String text) {
+    return text.text.gray400.size(20).make();
+  }
+
+  Widget buildSubtitleText(String text) {
+    return text.text.white.size(18).make().pOnly(bottom: 10);
+  }
+
+  Widget buildCharacterStatus() {
+    Icon statusIcon = Icon(
+      Icons.circle,
+      color: character.status == 'Alive' ? Colors.green : Colors.red,
+    );
+
+    if (character.status == 'unknown') {
+      statusIcon = Icon(
+        Icons.question_mark,
+        color: Colors.grey[400],
+        size: 18,
+      );
+    }
+
+    return Row(
+      children: [
+        statusIcon.pOnly(bottom: 10),
+        const WidthBox(10),
+        buildSubtitleText(character.status.capitalized),
+      ],
+    );
+  }
+
+  Widget buildSliverAppBar(context) {
     return SliverAppBar(
-      expandedHeight: 600,
+      expandedHeight: MediaQuery.of(context).size.height * 3 / 4,
       pinned: true,
       stretch: true,
-      backgroundColor: Colors.grey[900],
+      backgroundColor: Colors.amber[600],
       flexibleSpace: FlexibleSpaceBar(
         // title: character.name.text.gray900.make(),
         title: SizedBox(
@@ -38,7 +69,31 @@ class CharacterDetailsScreen extends StatelessWidget {
     return Scaffold(
       body: CustomScrollView(
         slivers: [
-          buildSliverAppBar(),
+          buildSliverAppBar(context),
+          SliverList(
+            delegate: SliverChildListDelegate([
+              Container(
+                margin: const EdgeInsets.fromLTRB(15, 15, 15, 0),
+                padding: const EdgeInsets.all(8),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    buildHeadlineText('Species:'),
+                    buildSubtitleText(character.species),
+                    buildHeadlineText('Gender:'),
+                    buildSubtitleText(character.gender),
+                    buildHeadlineText('Status:'),
+                    buildCharacterStatus(),
+                    buildHeadlineText('Last known location:'),
+                    buildSubtitleText(character.location['name']!),
+                    buildHeadlineText('Origin:'),
+                    buildSubtitleText(character.origin['name']!),
+                  ],
+                ),
+              ),
+              const HeightBox(500),
+            ]),
+          ),
         ],
       ),
     );
